d3c97e861517a73b42fec730bcea692b462884e4
Vitalii Khivrych <khivr.vitali@gmail.com>
2022-10-04 19:44:10 +0300

    add trycatch

--- contacts.js
+++ contacts.js
@@ -7,44 +7,57 @@ const updateContacts = async (contacts) =>
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
 async function listContacts() {
-  const result = await fs.readFile(contactsPath);
-  return JSON.parse(result);
+  try {
+    const result = await fs.readFile(contactsPath);
+    return JSON.parse(result);
+  } catch (err) {
+    console.error(err.message);
+  }
 }
 
 async function getContactById(contactId) {
-  const contacts = await listContacts();
-
-  const result = contacts.find(({ id }) => parseInt(id) === contactId);
-
-  return result || null;
+  try {
+    const contacts = await listContacts();
+    const result = contacts.find(({ id }) => parseInt(id) === contactId);
+    return result || null;
+  } catch (error) {
+    console.error(err.message);
+  }
 }
 
 async function removeContact(contactId) {
-  const contacts = await listContacts();
-  const index = contacts.findIndex(({ id }) => parseInt(id) === contactId);
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(({ id }) => parseInt(id) === contactId);
 
-  if (index === -1) {
-    return null;
+    if (index === -1) {
+      return null;
+    }
+
+    const [result] = contacts.splice(index, 1);
+    await updateContacts(contacts);
+    return result;
+  } catch (error) {
+    console.error(err.message);
   }
-
-  const [result] = contacts.splice(index, 1);
-  await updateContacts(contacts);
-  return result;
 }
 
 async function addContact(name, email, phone) {
-  const contacts = await listContacts();
+  try {
+    const contacts = await listContacts();
 
-  const newContact = {
-    id: (contacts.length + 1).toString(),
-    name,
-    email,
-    phone,
-  };
+    const newContact = {
+      id: (contacts.length + 1).toString(),
+      name,
+      email,
+      phone,
+    };
 
-  contacts.push(newContact);
-  await updateContacts(contacts);
-  return newContact;
+    await updateContacts([...contacts, newContact]);
+    return newContact;
+  } catch (error) {
+    console.error(err.message);
+  }
 }
 
 module.exports = {
